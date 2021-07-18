3ccf33344ad59b8334cd077057200a71eb5cd1fb
Pranitha Maganty <pranithamaganty@gmail.com>
2021-07-18 07:32:20 -0500

    add bootstrap to center of home page

--- portfolio/src/components/header/index.js
+++ portfolio/src/components/header/index.js
@@ -4,7 +4,7 @@ function Header() {
   return (
     <div className="row justify-content-center">
         <div className="col-sm-12 col-md-11 col-lg-11" id="header">
-            <div ClassName="row justify-content-center">
+            <div className="row justify-content-center">
                 <p>SOME RANDOM QUTOE" - PERSON</p>
             </div>
         </div>
--- portfolio/src/components/home/center/index.js
+++ portfolio/src/components/home/center/index.js
@@ -1,16 +1,27 @@
 import "./center.css";
 
 function Center() {
+
+  const profile_img = "./pran.PNG";
+
   return (
-    <div id="whole_center">
-        <div id="pic">
+    <div className="row justify-content-center">
+        <div className="col-sm-12 col-md-6 col-lg-6" id="whole_center">
+            <div className="row justify-content-center" id="pic">
+                <div className="col-sm-6 col-md-6 col-lg-6">
+                    <img src={profile_img} alt="PRAN" id="pran_img"></img>
+                </div>
+            </div>
+            <div className="row justify-content-center" id="name">
+                <div className="col-sm-6 col-md-6 col-lg-6">
+                    <p>PRANITHA MAGANTY</p>
+                </div>
+            </div>
+            <div className="row justify-content-center" id="menu">
+                <div className="col-sm-6 col-md-6 col-lg-6">
 
-        </div>
-        <div id="name">
-            
-        </div>
-        <div id="menu">
-
+                </div>
+            </div>
         </div>
     </div>
   );
