fbd9b9a08faed2530b1b4120ef45616c345984e4
Pranitha Maganty <pranithamaganty@gmail.com>
2021-07-18 09:25:23 -0500

    add faded background image to center home

--- portfolio/src/components/home/center/index.js
+++ portfolio/src/components/home/center/index.js
@@ -1,25 +1,34 @@
 import "./center.css";
+import background from "./web_bg_img.jpg";
+import pran from "./pran.PNG";
 
 function Center() {
 
   const profile_img = "./pran.PNG";
 
   return (
-    <div className="row justify-content-center">
-        <div className="col-sm-12 col-md-6 col-lg-6" id="whole_center">
-            <div className="row justify-content-center" id="pic">
-                <div className="col-sm-6 col-md-6 col-lg-6">
-                    <img src={profile_img} alt="PRAN" id="pran_img"></img>
+    <div className="center-wrap">
+        <img
+            class="center-bg"
+            src={background}
+            alt="background image"
+        />
+        <div className="row justify-content-center center-content" id="background">
+            <div className="col-sm-12 col-md-6 col-lg-6" id="whole_center">
+                <div className="row justify-content-center" id="pic">
+                    <div className="col-sm-6 col-md-6 col-lg-6">
+                        <img id="prof_img" src={pran} alt="PRAN" id="pran_img"></img>
+                    </div>
                 </div>
-            </div>
-            <div className="row justify-content-center" id="name">
-                <div className="col-sm-6 col-md-6 col-lg-6">
-                    <p>PRANITHA MAGANTY</p>
+                <div className="row justify-content-center" id="name">
+                    <div className="col-sm-6 col-md-6 col-lg-6">
+                        <p>PRANITHA MAGANTY</p>
+                    </div>
                 </div>
-            </div>
-            <div className="row justify-content-center" id="menu">
-                <div className="col-sm-6 col-md-6 col-lg-6">
+                <div className="row justify-content-center" id="menu">
+                    <div className="col-sm-6 col-md-6 col-lg-6">
 
+                    </div>
                 </div>
             </div>
         </div>
