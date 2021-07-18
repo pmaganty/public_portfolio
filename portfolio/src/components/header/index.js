6c271ec78755e43224789b3a240f60c45e201009
Pranitha Maganty <pranithamaganty@gmail.com>
2021-07-18 14:04:57 -0500

    center main page basic version done

--- portfolio/src/components/header/index.js
+++ portfolio/src/components/header/index.js
@@ -4,9 +4,6 @@ function Header() {
   return (
     <div className="row justify-content-center">
         <div className="col-sm-12 col-md-12 col-lg-12" id="header">
-            <div className="row justify-content-center">
-                <p>SOME RANDOM QUTOE" - PERSON</p>
-            </div>
         </div>
     </div>
   );
--- portfolio/src/components/home/center/index.js
+++ portfolio/src/components/home/center/index.js
@@ -4,8 +4,6 @@ import pran from "./pran.PNG";
 
 function Center() {
 
-  const profile_img = "./pran.PNG";
-
   return (
     <div className="center-wrap">
         <img
@@ -13,22 +11,21 @@ function Center() {
             src={background}
             alt="background image"
         />
-        <div className="row justify-content-center center-content" id="background">
-            <div className="col-sm-12 col-md-6 col-lg-6" id="whole_center">
+        <div className="row justify-content-center center-content">
+            <div className="col-sm-10 col-md-6 col-lg-6" id="whole_center">
                 <div className="row justify-content-center" id="pic">
-                    <div className="col-sm-6 col-md-6 col-lg-6">
-                        <img id="prof_img" src={pran} alt="PRAN" id="pran_img"></img>
+                    <div className="col-sm-10 col-md-10 col-lg-10">
+                        <img src={pran} alt="PRAN" className="img-responsive center-block d-block mx-auto" id="pran_img"></img>
                     </div>
                 </div>
-                <div className="row justify-content-center" id="name">
-                    <div className="col-sm-6 col-md-6 col-lg-6">
-                        <p>PRANITHA MAGANTY</p>
+                <div className="row justify-content-center">
+                    <div className="col-sm-12 col-md-12 col-lg-12" id="name">
+                        <h2>PRANITHA MAGANTY</h2>
                     </div>
                 </div>
-                <div className="row justify-content-center" id="menu">
-                    <div className="col-sm-6 col-md-6 col-lg-6">
-
-                    </div>
+                <div className="row" id="menu">
+                    <button class="nav_btn" typ="button">About Me</button>
+                    <button class="nav_btn" type="button">Contact</button>
                 </div>
             </div>
         </div>
