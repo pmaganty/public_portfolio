51e85037e75d861ae98467ba278ae24116ed34de
Pranitha Maganty <pranithamaganty@gmail.com>
2021-07-25 17:02:14 -0500

    add bootstrap structure and form to contact page

--- portfolio/src/components/contact/index.js
+++ portfolio/src/components/contact/index.js
@@ -1,13 +1,38 @@
 import "./contact.css";
+import Header from "../header";
+import Footer from "../footer";
 
-function About() {
+function Contact() {
   return (
-    <div className="row justify-content-center">
-        <div className="col-sm-12 col-md-12 col-lg-12">
-            <h1>CONTACT PAGE</h1>
+    <div>
+        <Header />
+        <div className="row justify-content-center">
+            <div className="col-sm-12 col-md-12 col-lg-12" id="contact_title">
+                <h1>Contact Me</h1>
+            </div>
         </div>
+        <div className="row">
+            <div className="col-sm-11 col-md-7 col-lg-7">
+                <form>
+                    <div className="form-group">
+                        <label for="exampleFormControlInput1">Name</label>
+                        <input type="email" className="form-control" id="exampleFormControlInput1"/>
+                    </div>
+                    <div className="form-group">
+                        <label for="exampleFormControlInput1">Email address</label>
+                        <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="name@example.com"/>
+                    </div>
+                    <div class="form-group">
+                        <label for="exampleFormControlTextarea1">Message</label>
+                        <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                    </div>
+                    <button type="submit" className="btn btn-primary">Submit</button>
+                </form>
+            </div>
+        </div>
+        <Footer />
     </div>
   );
 }
 
-export default About;
+export default Contact;
